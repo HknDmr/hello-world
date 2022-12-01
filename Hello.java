2ccc4a6bb52d6d767c95a46da017cbce8196fc96
Hakan <hkn-demir@hotmail.com>
2022-12-01 17:16:11 +0200

    First commit

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world. I am Hakan. Developer of the future."+
+                "This is my first project and coding");
+    }
+}
